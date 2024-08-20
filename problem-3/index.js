c6cda17ae31bc763429f6daadea06aa7b5b83da0
walid hasan <walid.official8@gmail.com>
2024-08-20 13:12:55 +0600

    problem 3 is remaining

---
+++ problem-3/index.js
@@ -0,0 +1,39 @@
+// 1. just print X = sum result;
+
+// let a = 10;
+// let b = 20;
+// let x = a + b;
+// console.log(`X = ${x}`);
+
+
+
+
+
+
+// 2. just print sum calculation 
+
+// let a = parseInt(prompt('Enter a number :'));
+// let b = parseInt(prompt('Enter a number :'));
+// let x = a + b;
+
+// console.log(`X = ${x}`);
+
+
+
+
+
+
+// 3. printing all calculation with prompt
+
+let a = parseInt(prompt('Enter Your First number :'));
+let c = parseInt(prompt('Enter your Second Number'));
+let b = parseInt(prompt('Enter Your Third number :'));
+
+let x = a + b;
+let y = a - b;
+let z = a * c;
+
+console.log(`X = ${x}`);
+console.log(`X = ${y}`);
+console.log(`X = ${z}`);
+
