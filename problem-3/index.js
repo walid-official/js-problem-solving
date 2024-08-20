c9d4e0e212a2033b2e13bc712aeba505ddc60150
walid hasan <walid.official8@gmail.com>
2024-08-20 14:12:33 +0600

    prompt calculator is done

--- problem-3/index.js
+++ problem-3/index.js
@@ -8,8 +8,6 @@
 
 
 
-
-
 // 2. just print sum calculation 
 
 // let a = parseInt(prompt('Enter a number :'));
@@ -25,15 +23,49 @@
 
 // 3. printing all calculation with prompt
 
+// let a = parseInt(prompt('Enter Your First number :'));
+// let c = parseInt(prompt('Enter your Second Number'));
+// let b = parseInt(prompt('Enter Your Third number :'));
+
+// let x = a + b;
+// let y = a - b;
+// let z = a * c;
+
+// console.log(`X = ${x}`);
+// console.log(`X = ${y}`);
+// console.log(`X = ${z}`);
+
+
+
+
+
+
+
+
+
+// 4. creating a calculator
+
 let a = parseInt(prompt('Enter Your First number :'));
-let c = parseInt(prompt('Enter your Second Number'));
-let b = parseInt(prompt('Enter Your Third number :'));
+let b = parseInt(prompt('Enter Your First number :'));
+let calculation = prompt("Enter Your calculation symbol : ");
 
-let x = a + b;
-let y = a - b;
-let z = a * c;
+let result;
 
-console.log(`X = ${x}`);
-console.log(`X = ${y}`);
-console.log(`X = ${z}`);
+switch(calculation){
+    case "+":
+        text = a + b;
+        break;
+    case "-":
+        text = a - b;
+        break;
+    case "*":
+        text = a * b;
+        break;
+    case "/":
+        text = a / b;
+        break;
+    default:
+        text = "No option is selected";
+}
 
+console.log(result);
