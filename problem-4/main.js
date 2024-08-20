f4c5e7595340d6197c051ad67b506eae4c0160a6
walid hasan <walid.official8@gmail.com>
2024-08-21 01:29:08 +0600

    new similer problem is solved

--- problem-4/main.js
+++ problem-4/main.js
@@ -1,8 +1,14 @@
-let number = parseInt(prompt("Enter a Number :"));
-let sum = 0;
-for(var i = 1; i <= number; i++){
-    sum += i*i;
-}
-console.log(sum);
+// let number = parseInt(prompt("Enter a Number :"));
+// let sum = 0;
+// for(var i = 1; i <= number; i++){
+//     sum += i*i;
+// }
+// console.log(sum);
 
-// 1+2+3+4+5+6+7+8+9
+let number = parseInt(prompt("Enter a Number :"));
+let product = 1;
+for(var i = 1; i <= number; i++){
+    product *= i;
+}
+
+console.log(product);
