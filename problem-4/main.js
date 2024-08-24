6105e8451e51a22e935dcbf51df2109ea35a2413
walid hasan <walid.official8@gmail.com>
2024-08-25 00:31:00 +0600

    practicing array map  method

--- problem-4/main.js
+++ problem-4/main.js
@@ -1,3 +1,5 @@
+// 1. Write a program to calculate the sum of all natural numbers from 1 to n, where n is provided by the user.
+
 // let number = parseInt(prompt("Enter a Number :"));
 // let sum = 0;
 // for(var i = 1; i <= number; i++){
@@ -5,10 +7,19 @@
 // }
 // console.log(sum);
 
-let number = parseInt(prompt("Enter a Number :"));
-let product = 1;
-for(var i = 1; i <= number; i++){
-    product *= i;
-}
 
-console.log(product);
+
+// 2. Write a program to calculate the factorial of a given number n. The factorial of a number is the product of all positive           integers less than or equal to that number.
+
+
+// let number = parseInt(prompt("Enter a Number :"));
+// let product = 1;
+// for(var i = 1; i <= number; i++){
+//     product *= i;
+// }
+// console.log(product);
+
+//
+
+// 3. Write a program that prints the multiplication table for a given number n (from 1 to 10).
+
---
+++ problem-5/index.js
@@ -0,0 +1,19 @@
+// array method practices
+
+// with simple function
+
+// const fruits = ['apple', 'banana', 'cherry'];
+// function myFruits(fruit){
+//     return fruit.toUpperCase();
+// }
+// const allFruits = fruits.map(myFruits);
+// console.log(allFruits);
+
+
+
+// with arrow function
+
+const fruits = ['apple', 'banana', 'cherry'];
+const uppercasedFruits = fruits.map(fruit => fruit.toUpperCase());
+
+console.log(uppercasedFruits); // Outputs: ['APPLE', 'BANANA', 'CHERRY']
